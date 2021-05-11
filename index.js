859d6e7b30cb504127c51b8b27176119b22ff478
chinorlando <linusistemintearti@hotmail.com>
2021-05-11 00:12:47 -0400

    Cambios sobre bandas terminado

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+// const port = process.env.PORT || 3000;
+
 // appa de express
 const app = express();
 
@@ -14,7 +16,46 @@ require('./sockets/socket');
 const publicpath = path.resolve(__dirname, 'public');
 app.use(express.static(publicpath));
 
-server.listen(process.env.PORT, (err) => {
+server.listen(process.env.PORT, "0.0.0.0", (err) => {
     if (err) throw new Error(err);
-    console.log('Servidor escuchando en el puerto:', 30000);
+    console.log('Servidor escuchando en el puerto:', process.env.PORT);
 });
+
+// server.listen(port, "0.0.0.0", () => {
+//   console.log("server started");
+//   console.log('Servidor escuchando en el puerto:', port);
+// });
+
+
+
+
+
+// const express = require("express");
+// var http = require("http");
+// const app = express();
+// const port = process.env.PORT || 3000;
+// var server = http.createServer(app);
+// var io = require("socket.io")(server);
+
+// //middlewre
+// app.use(express.json());
+// var clients = {};
+
+// io.on("connection", (socket) => {
+//   console.log("connetetd");
+//   console.log(socket.id, "has joined");
+//   socket.on("signin", (id) => {
+//     console.log(id);
+//     clients[id] = socket;
+//     console.log(clients);
+//   });
+//   socket.on("message", (msg) => {
+//     console.log(msg);
+//     let targetId = msg.targetId;
+//     if (clients[targetId]) clients[targetId].emit("message", msg);
+//   });
+// });
+
+// server.listen(port, "0.0.0.0", () => {
+//   console.log("server started");
+// });
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,24 @@
-const {io} = require('../index');
+const { io } = require('../index');
+
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Héroes del silencio'));
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Megadeth'));
+bands.addBand(new Band('Judas Priest'));
+
+// console.log(bands);
+
 
 // mensajes de socket
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -12,4 +28,33 @@ io.on('connection', client => {
         console.log('Escuchando', payload);
         io.emit('mensajeServ', {admin: 'Nuevo mensaje'});
     });
+
+    client.on('este-mensaje', ( payload ) => {
+        io.emit('nuevo-mensaje', payload);
+    });
+
+    client.on('mimensaje', (payload) => {
+        console.log(payload);
+        client.broadcast.emit('nuevo-mensaje', payload);
+    });
+
+	client.on('vote-band', (payload) => {
+        // console.log(payload.id);
+        bands.voteBand(payload.id);
+       	// bands.getBands();
+       	// console.log(bands);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+
 });
